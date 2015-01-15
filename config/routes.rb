1d0a038bb0294c21dd0cbbc0beb81b0a2c5c3065
Dan Oliphant <danoliphant@mac.com>
2015-01-15 17:55:17 -0600

    Crud and posts index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcome/contact'
 
-  get 'welcome/index'
+  resources :posts
 
-  get 'welcome/about'
+#I can't remember if I made this for an extra assignment. May have to delete.
+#  get 'welcome/contact'
+
+  get 'about' => 'welcome#index'
 
   root to:'welcome#index'
-
 
 
 end
