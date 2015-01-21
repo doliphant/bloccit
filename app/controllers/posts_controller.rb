b99c74ba03ae49bba822dd4dfef5de2c647be87d
Dan Oliphant <danoliphant@mac.com>
2015-01-21 12:18:30 -0600

    Email, edit, and refactoring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 
     def create
       @topic = Topic.find(params[:topic_id])
-      @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+      @post = current_user.posts.build(post_params)
       @posts.topic = @topic
         authorize @post
 
@@ -33,18 +33,25 @@ class PostsController < ApplicationController
       authorize @post
   end
 
-    def update
-      @topic = Topic.find(params[:toic_id])
-      @post = Post.find(params[:id])
-        authorize @post
+  def update
+    @topic = Topic.find(params[:toic_id])
+    @post = Post.find(params[:id])
+      authorize @post
 
-      if @post.update_attributes(params.require(:post).permit(:title, :body))
-        flash[:notice] = "Post was updated."
-        redirect_to [@topic, @post]
-      else
-        flash[:error] = "There was an error saving the post. Please try again."
-        render :edit
-      end
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post was updated."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 
 end
