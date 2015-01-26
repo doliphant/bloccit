f0f4d83ac7a19b4704dadbf3d967a1238e0016e2
Dan Oliphant <danoliphant@mac.com>
2015-01-26 17:53:19 -0600

    Mocking, factories, rspec

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,10 +9,9 @@ class Post < ActiveRecord::Base
 
     validates :title, length: {minimum: 5}, presence: true
     validates :body, length: {minimum: 20}, presence: true
-    # validates :topic, presence: true
-    # validates :user, presence: true
+    validates :topic, presence: true
+    validates :user, presence: true
 
-    after_create :create_vote
 
 
     def up_votes
@@ -34,12 +33,8 @@ class Post < ActiveRecord::Base
       update_attribute(:rank, new_rank)
     end
 
-  private
-
-
     def create_vote
       user.votes.create( post: self, value: 1)
-
     end
 
 end
---
+++ spec/controllers/votes_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+include Devise::TestHelpers
+include TestFactories
+
+describe VotesController do
+
+  describe '#upvote' do
+    it "adds an up-vote to the post" do
+      request.env["HTTP_REFERER"] = '/'
+      @user = authenticated_user
+      @post = associated_post
+      sign_in @user
+
+      expect {
+        post( :up_vote, post_id: @post.id)
+      }.to change{@post.up_votes}.by 1
+    end
+  end
+end
