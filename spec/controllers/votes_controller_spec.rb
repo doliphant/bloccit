9b631239566059a13bd4753724f121f22e4398a1
Dan Oliphant <danoliphant@mac.com>
2015-01-26 18:05:00 -0600

    mocking, factories, includes downvote spec

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -5,16 +5,27 @@ include TestFactories
 
 describe VotesController do
 
+  before do
+    request.env["HTTP_REFERER"] = '/'
+    @user = authenticated_user
+    @post = associated_post
+    sign_in @user
+  end
+
   describe '#upvote' do
     it "adds an up-vote to the post" do
-      request.env["HTTP_REFERER"] = '/'
-      @user = authenticated_user
-      @post = associated_post
-      sign_in @user
-
       expect {
         post( :up_vote, post_id: @post.id)
       }.to change{@post.up_votes}.by 1
     end
   end
+
+  describe '#downvote' do
+    it "adds a down_vote to the post" do
+      expect {
+        post( :down_vote, post_id: @post.id)
+      }.to change{@post.down_votes}.by 1
+    end
+  end
+
 end
